aa5482e32004cdc9ede179d0f62b301abd1fa4ce
horie-toshiya <mmddgpcd1122@gmail.com>
2021-07-19 02:17:08 +0000

    [Update]users/マイページレイアウト

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+
+Refile.secret_key = '7b7b27feb7ad7d3fb3ade49983ccdb99b60ba2d14cd9df86f69a8ffd2bed4a730253036961f115032a6214653ba19ba0065dd7b3a7d45bd7c051ccffbd536199'
